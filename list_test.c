d964bcbba379cd4ec77f104b99fd2bb44ef51806
igore <igor.erokhin@gmail.com>
2013-11-09 21:46:58 +0100

    Separate tests into functions.

--- list_test.c
+++ list_test.c
@@ -9,10 +9,15 @@ char* months[M_SIZE] = {"January", "February", "March", "April",
                 "May", "June", "July", "August", "September",
                 "October", "November", "December"
                 };
-static void print_months();
 
-int main(int argc, char **argv){
-    printf("Hello Single Link List\n");
+static void print_months(){
+    int i;
+    for(i = 0; i < M_SIZE; i++){
+        printf("%s\n", months[i]);
+    }
+}
+
+static int test_insert_back(){
     sll_node_t* list = NULL;
     int i;
 
@@ -20,22 +25,22 @@ int main(int argc, char **argv){
         sll_node_t* node = new_node();
         if(node == NULL){
             printf("Could not create a new node\n");
-            exit(EXIT_FAILURE);
+            return EXIT_FAILURE;
         }
         node->data = strdup(months[i]);
         node->size = strlen(months[i]);
         insert_back(&list, node);
     }
     clear_list(&list);
-    if(list == NULL){
-        printf("List is empty\n");
+    if(list != NULL){
+        printf("Expected empty list, actual list is not empty\n");
+        return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
 }
 
-static void print_months(){
-    int i;
-    for(i = 0; i < M_SIZE; i++){
-        printf("%s\n", months[i]);
-    }
+int main(int argc, char **argv){
+    printf("Hello Single Link List\n");
+    test_insert_back();
+    return EXIT_SUCCESS;
 }
